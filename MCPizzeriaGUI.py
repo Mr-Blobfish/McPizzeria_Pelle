84390a8693f2948d54daab1ce16c0c5c2f1c81a7
Mr-Blobfish <pelleblobvis@gmail.com>
2026-04-07 09:28:53 +0200

    commit 1.3

--- MCPizzeriaGUI.py
+++ MCPizzeriaGUI.py
@@ -11,20 +11,8 @@
 ### --------- Bibliotheken en globale variabelen -----------------
 from tkinter import *
 import MCPizzeriaSQL
-import sqlite3
-with sqlite3.connect("MCPizzeria.db") as db:
- #cursor is object waarmee je data uit de database kan halen
- cursor = db.cursor()
 
 ### ---------  Functie definities  -----------------
-def maakTabellenAan():
- # Maak een nieuwe tabel met 3 kolommen: id, naam, prijs
- cursor.execute("""
- CREATE TABLE IF NOT EXISTS tbl_pizzas(
- gerechtID INTEGER PRIMARY KEY AUTOINCREMENT,
- gerechtNaam TEXT NOT NULL,
- gerechtPrijs REAL NOT NULL);""")
- print("Tabel 'tbl_pizzas' aangemaakt.")
 
 
 ### --------- Hoofdprogramma  ---------------
--- MCPizzeriaSQL.py
+++ MCPizzeriaSQL.py
@@ -8,14 +8,22 @@
 
 
 ### --------- Bibliotheken en globale variabelen -----------------
-
 import sqlite3
 with sqlite3.connect("MCPizzeria.db") as db:
-    cursor = db.cursor()#cursor is object waarmee je data uit de database kan halen
+ #cursor is object waarmee je data uit de database kan halen
+ cursor = db.cursor()
 
 
 ### ---------  Functie definities  -----------------
 
 
-### --------- Hoofdprogramma  ---------------
 
+### --------- Hoofdprogramma  ---------------
+def maakTabellenAan():
+ # Maak een nieuwe tabel met 3 kolommen: id, naam, prijs
+ cursor.execute("""
+ CREATE TABLE IF NOT EXISTS tbl_pizzas(
+ gerechtID INTEGER PRIMARY KEY AUTOINCREMENT,
+ gerechtNaam TEXT NOT NULL,
+ gerechtPrijs REAL NOT NULL);""")
+ print("Tabel 'tbl_pizzas' aangemaakt.")
